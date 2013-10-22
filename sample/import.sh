572c3acd80f9ae66195c5d9bd2806744f312d667
drtobbe <drtobbe@me.com>
2013-10-22 23:03:16 +0200

    sample

--- sample/import.sh
+++ sample/import.sh
@@ -6,5 +6,6 @@ rm -rf target/graph.db
 
 java -server -Xmx1G -jar target/batch-import-jar-with-dependencies.jar \
 target/graph.db sample/nodes.csv sample/edges.csv \
-node_index Concepts exact sample/nodes_index.csv
+node_index Concepts exact sample/nodes_index.csv \
+node_index __types__ exact sample/nodes_types.csv
 
